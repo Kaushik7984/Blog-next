473f197da99f5ef97a16242eb20ebffc39135d7a
Kaushik Tapaniya <113747930+Kaushik7984@users.noreply.github.com>
2025-03-27 12:00:25 +0530

    Update auth-form.js

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -14,6 +14,7 @@ const createUser = async (email, password) => {
   });
 
   const data = await response.json();
+  //console.log(data)
 
   if (!response.ok) {
     throw new Error(data.message || "Something went wrong");
